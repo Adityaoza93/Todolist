1f38d36e8eaedfcf2dea5c4be3b6ea2fe9132a13
Adityaoza93 <aditya.oza@vit.edu.in>
2022-10-12 18:50:51 +0530

    first

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            Created by Aditya Oza for task 1 of internship at LetsGrowMore
+            
+        </div>
+    )
+}
